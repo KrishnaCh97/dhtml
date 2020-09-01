e26ca6b82037aab74df2c8cf2e74d9367ddcc71f
KrishnaCh97 <chskbharath@gmail.com>
2020-09-01 11:47:16 +0530

    Printing task

---
+++ new.js
@@ -0,0 +1,9 @@
+var n1 = prompt('Enter 1st number!');
+var nu1 = parseInt(n1);
+var n2 = prompt('Enter 2nd number!');
+var nu2 = parseInt(n2);
+var n3 = nu1+nu2; 
+
+document.getElementById('num1').innerHTML = n1;
+document.getElementById('num2').innerHTML = n2;
+document.getElementById('sum').innerHTML = n3; 
