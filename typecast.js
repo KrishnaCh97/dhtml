729707104a7f3ee959428662168de83b78f02d26
KrishnaCh97 <chskbharath@gmail.com>
2020-09-01 12:07:35 +0530

    typecast

---
+++ typecast.js
@@ -0,0 +1,3 @@
+var arr = [1,2,3,4,5];
+arr[3] = "4";
+document.getElementById('typec').innerHTML = typeof arr[0] + " <br>" + typeof arr[1] + "<br> " + typeof arr[2] + "<br> " + typeof arr[3] + " <br>" + typeof arr[4]; 
