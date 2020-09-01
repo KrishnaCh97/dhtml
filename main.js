66f7712ea017e976eb98bc09ce1e826a7742bff6
KrishnaCh97 <chskbharath@gmail.com>
2020-09-01 18:34:23 +0530

    regex

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ let myButton = document.querySelector('button');
 let myPara = document.querySelector('p');
 function setUserName(){
 	let myName = prompt('Please enter your name!');
+	var patt = /[a-zA-z]/g;
+	var name = myName.match(patt);
 	localStorage.setItem('name',myName);
-	myPara.textContent = 'You are awesome,' + myName;
+	myPara.textContent = 'You are awesome,' + name;
 }
-myButton.onclick = setUserName();
+myButton.onclick = setUserName;
